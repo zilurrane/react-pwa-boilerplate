f81f509ab06bb4d764166c58abb61cf9b5ea83ae
zilurane <zilurane@outlook.com>
2018-10-28 19:27:27 +0530

    Added collapse

--- src/App.js
+++ src/App.js
@@ -61,49 +61,80 @@ class App extends Component {
                 <div className='col text-center mt-5'>
                     <h1>React PWA</h1>
                     <h6>
-                        v1.1
+                        v1.2
                     </h6>
                     <h1 className='text-danger'>&hearts;</h1>
-                    {'share' in navigator && <button class="btn btn-default btn-block" onClick={() => this.share()}>Share</button>}
+                    {'share' in navigator && <button className="btn btn-default btn-block" onClick={() => this.share()}>Share</button>}
                 </div>
             </div>
-            {
-                this.state.quotes.map((quote, key) => {
-                    return <div key={key} className='row text-center'>
-                        <div className='col'>
-                            <div className='card bg-dark shadow-sm'>
-                                <div className='card-body'>
-                                    <div className='row'>
-                                        <div className='col-sm-10 p-1'>
-                                            <h6>{quote.text}</h6>
-                                        </div>
-                                        <div className='col-sm-2'>
-                                            <button className='btn btn-default' onClick={() => this.displayNotification(quote.text)}>Notify</button>
+
+            <div className="accordion" id="accordionPWA">
+                <div className="card bg-dark shadow-sm">
+                    <div className="card-header" id="headingOne">
+                        <h5 className="mb-0">
+                            <button className="btn btn-dark btn-block" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                <b>Quotes</b>
+                            </button>
+                        </h5>
+                    </div>
+
+                    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionPWA">
+                        <div className="card-body">
+                            {
+                                this.state.quotes.map((quote, key) => {
+                                    return <div key={key} className='row text-center'>
+                                        <div className='col'>
+                                            <div className='card bg-dark shadow-sm'>
+                                                <div className='card-body'>
+                                                    <div className='row'>
+                                                        <div className='col-sm-10 p-1'>
+                                                            <h6>{quote.text}</h6>
+                                                        </div>
+                                                        <div className='col-sm-2'>
+                                                            <button className='btn btn-default' onClick={() => this.displayNotification(quote.text)}>Notify</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                })
+                            }
+
                         </div>
                     </div>
-                })
-            }
-            <div className='row'>
-                <div className='col'>
-                    <div className='card bg-dark shadow-sm'>
-                        <div className='card-body'>
+                </div>
+                <div className="card bg-dark shadow-sm">
+                    <div className="card-header" id="headingTwo">
+                        <h5 className="mb-0">
+                            <button className="btn btn-dark btn-block collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                <b>Camera</b>
+                            </button>
+                        </h5>
+                    </div>
+                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionPWA">
+                        <div className="card-body">
                             <div className='row'>
-                                <div className='col-sm-12 text-center'>
-                                    <video style={{ height: '100%', width: '100%' }} id='player' ref={player => this.player = player} autoPlay />
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <div className='col-sm-12'>
-                                    <button onClick={() => this.captureImage()} className='btn btn-default btn-block' id='capture'>Capture</button>
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <div className='col-sm-12 text-center'>
-                                    <canvas style={{ height: '100%', width: '100%' }} id='canvas' ref={canvas => this.canvas = canvas} />
+                                <div className='col'>
+                                    <div className='card bg-dark shadow-sm'>
+                                        <div className='card-body'>
+                                            <div className='row'>
+                                                <div className='col-sm-12 text-center'>
+                                                    <video style={{ height: '100%', width: '100%' }} id='player' ref={player => this.player = player} autoPlay />
+                                                </div>
+                                            </div>
+                                            <div className='row'>
+                                                <div className='col-sm-12'>
+                                                    <button onClick={() => this.captureImage()} className='btn btn-default btn-block' id='capture'>Capture</button>
+                                                </div>
+                                            </div>
+                                            <div className='row'>
+                                                <div className='col-sm-12 text-center'>
+                                                    <canvas style={{ height: '100%', width: '100%' }} id='canvas' ref={canvas => this.canvas = canvas} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
